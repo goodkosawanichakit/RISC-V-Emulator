1b8857a08e631a1c41245ed34a067182fb3fc5cb
goodkosawanichakit <goodkosawanichakit@gmail.com>
2025-12-06 20:46:15 +0700

    What

--- src/main.c
+++ src/main.c
@@ -2,21 +2,7 @@
 #include <stdio.h>
 
 int main() {
-  RISCV_CPU cpu = {0};
-  cpu.regs[1] = 5;
-
-  printf("--- Simulation Start ---\n");
-
-  // Load Program
-  // ADDI: x2 = x1 + 10
-  uint32_t inst_addi = 0x00A08113;
-  cpu_exec(&cpu, inst_addi);
-
-  // uint32_t inst_jal = ...;
-  // cpu_exec(&cpu, inst_jal);
-
-  printf("--- Simulation End ---\n");
-  printf("x1: %d, x2: %d\n", cpu.regs[1], cpu.regs[2]);
+  RISCV_CPU *cpu;
 
   return 0;
 }
